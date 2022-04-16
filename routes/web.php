d5244f289495d07a6119124519620748e9a83f72
Charlie Tomas <78902076+Crtomas2@users.noreply.github.com>
2022-04-16 14:42:41 +0800

    Updated route.php

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PromodiserController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
 use function GuzzleHttp\Promise\task;
@@ -31,4 +32,4 @@ Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
 
 Route::resource('/promodisers', PromodiserController::class);
 
-Route::resource('tasks',TaskController::class);
+Route::resource('tasks', [App\Http\ControllersTaskController::class]);
