8b08ddf99ef2ff8ac68b80e5d249055e4782d295
Crtomas2 <charlietomas88@gmail.com>
2022-07-05 09:20:50 +0800

    update token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -19,7 +19,8 @@ class VerifyCsrfToken extends Middleware
 
         'dashboard/*',
         'register/*',
-        'http://edgescanner.herokuapp.com/register'
+        'http://edgescanner.herokuapp.com/register',
+        'http://edgescanner.herokuapp.com/login',
 
     ];
 }
